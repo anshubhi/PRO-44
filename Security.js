1c547bb22fcd0b5b920c7a6286278151f21176dd
anshubhi <83507280+anshubhi@users.noreply.github.com>
2021-08-20 22:52:54 +0530

    Add files via upload

---
+++ Security.js
@@ -0,0 +1,39 @@
+class Security {
+
+    constructor(){
+
+        // Add code to create the input and button elements
+this.access1 = createInput("Code 1");
+this.access1.position(100,90);
+this.access1.style('background','white');
+
+this.button1 = createButton('Check');
+this.button1.position(100,120);
+this.button1.style('background','lightgrey');
+
+authenticate(actualCode,enteredCode);
+if(actualCode === enteredCode){
+    return true
+}
+else{
+    return false
+}
+
+    }
+
+    display(){
+
+        // Add code to make the buttons function as expected
+        this.button1.mousePressed(()=> {
+
+            if(system.authenticate(accessCode1,this.access1.value())){
+
+                this.button1.hide();
+                this.access1.hide();
+                score++;
+            }
+        })
+
+
+    }
+}
